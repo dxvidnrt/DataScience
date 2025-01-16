81acae8798e637fdeca7628ae476c2758e082e1b
David Niebert <david.niebert@googlemail.com>
2025-01-16 13:37:07 +0100

    Finished Task 5

--- assign2/distributed_data_processing/haters.py
+++ assign2/distributed_data_processing/haters.py
@@ -6,16 +6,22 @@ from mrjob.protocol import TextValueProtocol
 
 
 class Haters(MRJob):
+    """
+    Why it would be nice to have combiners for this task:
+    Using a combiner to accumelate all ratings below 2 starts, minimizes the communication between mapper and reducer.
+    In a best case scenario, a mapper maps all ratings of the same user, effectively reducing the network traffic from |ratings below 2| to     1.
+    """
 
 
     def mapper(self, _, line: str):
         i, user_id, movie_id, rating, timestamp, rating_normalized = line.split(',')
-        # TODO
-        ...
+        if float(rating) < 2:
+            yield user_id, 1
 
     def reducer(self, key, values):
-        # TODO
-        ...
+        user_id = key
+        if sum(values) > 50:
+            yield None, user_id
 
 
 if __name__ == '__main__':
